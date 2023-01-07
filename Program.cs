5cb1545a418cc82460d8fe9d06eeed75066eb879
Lisaruru <bilberry2009@yandex.ru>
2023-01-07 17:20:47 +0800

    add code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ 
+string[] array =
+{
+    "Привет","22", "(8)", "much", "&&", "z"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+ Console.WriteLine(string.Join(",", result, 0, realSize));
